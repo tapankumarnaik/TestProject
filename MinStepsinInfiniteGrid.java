64325dea3ec2f3a75c73a116143e8bd08706940d
tapankumarnaik <tapankumarnaik17@gmail.com>
2017-01-06 22:31:48 +0530

    New file
    You are in an infinite 2D grid where you can move in any of the 8 directions :
     (x,y) to 
        (x+1, y), 
        (x - 1, y), 
        (x, y+1), 
        (x, y-1), 
        (x-1, y-1), 
        (x+1,y+1), 
        (x-1,y+1), 
        (x+1,y-1) 
    You are given a sequence of points and the order in which you need to cover the points. Give the minimum number of steps in which you can achieve it. You start from the first point.
    
    Example :
    
    Input : [(0, 0), (1, 1), (1, 2)]
    Output : 2
    It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).
    
    This question is intentionally left slightly vague. Clarify the question by trying out a few cases in the “See Expected Output” section.

---
+++ MinStepsinInfiniteGrid.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package array.math;
+
+import java.util.ArrayList;
+
+/**
+ * @author SURAJ/Tapan
+ *
+ */
+public class MinStepsinInfiniteGrid {
+	
+	public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list1.add(2);
+        list2.add(1);
+        list1.add(5);
+        list2.add(5);
+        MinStepsinInfiniteGrid minStepsinInfiniteGrid = new MinStepsinInfiniteGrid();
+        System.out.println(minStepsinInfiniteGrid.process(list1,list2));
+    }
+
+    private int process(ArrayList<Integer> X, ArrayList<Integer> Y) {
+        if(X.size() ==0 || Y.size() ==0 || (X.size() != Y.size())){
+            throw new IllegalArgumentException();
+        }else {
+            int dist = 0;
+            for (int i = 1; i < X.size(); i++) {
+                    dist+= calDistanceBWPoints(X.get(i-1),Y.get(i-1),X.get(i),Y.get(i));
+            }
+
+            return dist;
+        }
+    }
+
+    private int calDistanceBWPoints(int x1, int y1, int x2, int y2) {
+        int diffOfXCoOrdinates = Math.abs(x1 - x2);
+        int diffOfYCoOrdinates = Math.abs(y1 - y2);
+        return Math.max(diffOfXCoOrdinates, diffOfYCoOrdinates);
+    }
+}
+
+
